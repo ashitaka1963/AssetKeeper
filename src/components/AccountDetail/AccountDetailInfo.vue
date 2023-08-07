36e4efec2d721de063e23c1b4b307dc389d83d8c
ashitaka1963 <ashitaka1963@gmail.com>
2023-08-07 18:11:01 +0900

    feat: 【AccountDetailView】アカウント情報のバックエンド連携

--- src/components/AccountDetail/AccountDetailInfo.vue
+++ src/components/AccountDetail/AccountDetailInfo.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { computed } from 'vue';
+import { useUsersStore } from '@/stores/users';
 import { useAccountsStore } from '@/stores/accounts';
 
 interface Props {
-  accountId: number;
+  accountId: string;
 }
 
 const props = defineProps<Props>();
+const usersStore = useUsersStore();
 const accountsStore = useAccountsStore();
 
 // ========================================
@@ -16,6 +18,12 @@ const accountsStore = useAccountsStore();
 const account = computed((): any => {
   return accountsStore.getById(props.accountId);
 });
+
+const userName = computed((): any => {
+  return usersStore.getById(account.value.ownerId).userName;
+});
+
+console.log(account);
 </script>
 
 <template>
@@ -28,18 +36,18 @@ const account = computed((): any => {
         <el-text tag="p" class="black-text">Type</el-text>
       </el-col>
       <el-col :span="8">
-        <el-text tag="p" class="black-text">User</el-text>
+        <el-text tag="p" class="black-text">Owner</el-text>
       </el-col>
     </el-row>
     <el-row>
       <el-col :span="8">
-        <el-text tag="p">{{ account.name }}</el-text>
+        <el-text tag="p">{{ account.accountName }}</el-text>
       </el-col>
       <el-col :span="8">
-        <el-text tag="p">{{ account.type }}</el-text>
+        <el-text tag="p">{{ account.accountType }}</el-text>
       </el-col>
       <el-col :span="8">
-        <el-text tag="p">{{ account.user }}</el-text>
+        <el-text tag="p">{{ userName }}</el-text>
       </el-col>
     </el-row>
   </div>
