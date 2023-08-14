9ef4864701e0aa0ded7805532bcff08cce9b4d46
ashitaka1963 <ashitaka1963@gmail.com>
2023-08-15 08:06:17 +0900

    feat: Loading追加#3

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -8,6 +8,8 @@ import DashboardAccounts from '../components/Dashboard/DashboardAccounts.vue';
 import { useAccountsStore } from '@/stores/accounts';
 import { useUsersStore } from '@/stores/users';
 
+import loadingUtils from '../CustomLoading';
+
 const usersStore = useUsersStore();
 const accountsStore = useAccountsStore();
 
@@ -19,8 +21,12 @@ init();
 // Methods
 // ========================================
 async function init() {
+  loadingUtils.startLoading();
+
   await getAccounts();
   await getUsers();
+
+  loadingUtils.closeLoading();
 }
 
 function getAccounts() {
